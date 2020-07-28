1dd85b163fbac963d45368f08d0c74de1f718c3f
Leo Longhi <leo10tibia@hotmail.com>
2020-07-27 23:43:49 -0300

    React JS Project

--- Chat.js
+++ Chat.js
@@ -3,6 +3,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = 5571;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +13,6 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
